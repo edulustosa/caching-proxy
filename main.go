cec1d7e307db239fda134a3303fb1357ca832600
Eduardo <eduardolustosa26@gmail.com>
2024-10-10 21:10:47 -0300

    feat: simple caching

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
-)
 
-type ProxyConfig struct {
-	Port     int
-	Origin   string
-	RedisURL *string
-}
+	"github.com/edulustosa/caching-proxy/memory"
+)
 
 func main() {
 	port := flag.Int("port", 3000, "Port to listen on")
@@ -23,35 +21,38 @@ func main() {
 
 	flag.Parse()
 
-	var proxy ProxyConfig
-	proxy.Port = *port
-
-	if *origin == "" {
-		fmt.Fprintln(os.Stderr, "origin is required")
-		os.Exit(1)
-	}
-
-	_, err := url.Parse(*origin)
+	originURL, err := url.Parse(*origin)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "origin must be a valid url")
 		os.Exit(1)
 	}
 
-	proxy.Origin = *origin
-
+	var proxy http.HandlerFunc
 	if *redisURL != "" {
-		_, err := url.Parse(*redisURL)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "redis-url must be a valid url")
-			os.Exit(1)
-		}
-		proxy.RedisURL = redisURL
+		// TODO
+	} else {
+		cache := memory.NewCache()
+		proxy = handleRequest(originURL, cache)
 	}
 
-	fmt.Println(proxy)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", *port),
+		Handler: http.HandlerFunc(proxy),
+	}
+
+	log.Printf("Starting proxy server on port %d", *port)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
 }
 
 var customTransport = http.DefaultTransport
+
+type Response struct {
+	StatusCode int
+	Header     map[string][]string
+	Body       []byte
+}
 
 type cache interface {
 	Get(ctx context.Context, key string) ([]byte, error)
@@ -59,34 +60,53 @@ type cache interface {
 	Clear(ctx context.Context) error
 }
 
-func handleRequest(targetURL *url.URL, cache cache) http.HandlerFunc {
+func handleRequest(origin *url.URL, cache cache) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		proxyReq, err := http.NewRequest(r.Method, targetURL.String(), r.Body)
+		targetURL := origin.String() + r.URL.RequestURI()
+
+		data, err := cache.Get(r.Context(), targetURL)
 		if err != nil {
-			http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
-			return
-		}
-
-		for name, value := range r.Header {
-			for _, v := range value {
-				proxyReq.Header.Add(name, v)
+			proxyReq, err := http.NewRequest(r.Method, targetURL, r.Body)
+			if err != nil {
+				http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
+				return
 			}
-		}
 
-		resp, err := customTransport.RoundTrip(proxyReq)
-		if err != nil {
-			http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		for name, values := range resp.Header {
-			for _, value := range values {
-				w.Header().Add(name, value)
+			for name, value := range r.Header {
+				for _, v := range value {
+					proxyReq.Header.Add(name, v)
+				}
 			}
+
+			resp, err := customTransport.RoundTrip(proxyReq)
+			if err != nil {
+				http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
+				return
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				http.Error(w, "Error reading proxy response", http.StatusInternalServerError)
+				return
+			}
+
+			if err := cache.Set(r.Context(), targetURL, body); err != nil {
+				log.Printf("Error caching response: %v", err)
+			}
+
+			for name, values := range resp.Header {
+				for _, value := range values {
+					w.Header().Add(name, value)
+				}
+			}
+
+			w.Header().Set("X-Cache", "MISS")
+			w.WriteHeader(resp.StatusCode)
+			io.Copy(w, bytes.NewReader(body))
 		}
 
-		w.WriteHeader(resp.StatusCode)
-		io.Copy(w, resp.Body)
+		w.Header().Set("X-Cache", "HIT")
+		io.Copy(w, bytes.NewReader(data))
 	}
 }
